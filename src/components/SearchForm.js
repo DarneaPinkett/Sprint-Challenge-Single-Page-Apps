152512c5ab694b68aef20b80a6b97d3913c6edd8
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-01-25 13:05:00 -0500

    SearchForm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
+  const [results, setResults] = useState();
+
+  const handleChanges = e => {
+    setResults(e.target.value);
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const filterd = props.characters.filter(char => {
+      return char.name.toLowerCase().indexOf(results.toLowerCase()) !== -1;
+    });
+    props.search(filterd);
+    console.log(filterd);
+  };
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <form onSubmit={submitHandler}>
+       <input type="text" name="character" placeholder="Search By Name" onChange={handleChanges}></input>
+     </form>
     </section>
   );
 }
