7a7b7b0fde62700b493d95658e43100c76ab8014
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-01-25 12:41:26 -0500

    App+CCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div>
+      <div>ID: {props.character.id}</div>
+      <div>Name: {props.character.name}</div>
+      <div>Gender: {props.character.gender}</div>
+      <div>Species: {props.character.species}</div>
+      <div>Origin: {props.character.origin.name}</div>
+      <div>Status: {props.character.status}</div>
+    </div>
+  )
 }
