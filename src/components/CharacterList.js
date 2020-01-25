4066be74a68ac59cd2400fdf36d3abf25cb32e36
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-01-25 12:55:35 -0500

    CharacterList

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from "react";
+import Axios, {Link} from "axios";
+import CharacterCard from "./CharacterCard.js";
+import SearchForm from "./SearchForm.js";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+  const [filiterdData, updateData] = useState([]);
+
+  const search = charArr => {
+    updateData(charArr)
+  };
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios.get("https://rickandmortyapi.com/api/character/")
+    .then(response => {console.log(response.data.results);
+    setCharacters(response.data.results);
+    updateData(response.data.results);
+  });
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>List of Characters</h2>
+      <Link to={"/"}>Home</Link>
+      <SearchForm search={search} characters={characters} />
+      {filiterdData.map(char => {
+        return <CharacterCard key={char.id} character={char} />;
+      })}
     </section>
   );
 }
